36ef561c0ca795197c601900f4a2adeb4145c09b
Aaron Fisher <aaronfisher@MacBook-Pro-20.local>
2018-10-29 09:51:07 -0500

    Initial Commit

---
+++ VideoGameLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoGameLibrary
+//
+//  Created by Aaron Fisher on 10/29/18.
+//  Copyright © 2018 Aaron Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
